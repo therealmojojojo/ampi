df3dfc95a11c1c443f131c5b88745822fe6b43c9
mojojojo <costin.genescu@gmail.com>
2019-11-27 00:50:12 +0200

    stuff i cannot describe yet

--- commands/show_clock.py
+++ commands/show_clock.py
@@ -4,6 +4,7 @@ import sys
 import os
 picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'img')
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+fontdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'fonts')
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
@@ -15,40 +16,47 @@ import traceback
 
 logging.basicConfig(level=logging.DEBUG)
 
+def start_centered_text(text_len, font_size):
+    return round((296 - (text_len * font_size)) / 2)
+
+
+# 296*128
+
 try:
     logging.info("epd2in9 Demo")
     
     epd = epd2in9.EPD()
-    logging.info("init and Clear")
+    logging.info("init")
+    epd.init(epd.lut_full_update)
+    logging.info("Clear")
+    epd.Clear(0xFF)
+    logging.info("Start sleeping")
+    time.sleep(2)
+
+    logging.info("Loading fonts")
+    font48 = ImageFont.truetype(os.path.join(fontdir, 'Anonymous.ttf'), 48)
+    font36 = ImageFont.truetype(os.path.join(fontdir, 'Anonymous.ttf'), 36)
+    font24 = ImageFont.truetype(os.path.join(fontdir, 'Anonymous.ttf'), 24)
+    font18 = ImageFont.truetype(os.path.join(fontdir, 'Anonymous.ttf'), 18)
+    logging.info("End loading fonts")
+    
+  
+    time_image = Image.new('1', (epd.height, epd.width), 255)
+    time_draw = ImageDraw.Draw(time_image)
+#    time_draw.rectangle((35, 35, 130, 80), fill = 255)
+    time_draw.line((1, 1, 1, 129), fill = 0)
+    text = time.strftime('%A, %d %b')
+    text_len = len(text)
+    start = start_centered_text(text_len, 24)
+    logging.info("start=" + str(start))
+    logging.info("text_len=" + str(text_len))
+    time_draw.text((start if start > 0 else 10, 10), text, font = font24, fill = 0)
+    time_draw.text((65, 40), time.strftime('%H:%M'), font = font48, fill = 0)
+    epd.display(epd.getbuffer(time_image))
+    
     epd.init(epd.lut_full_update)
     epd.Clear(0xFF)
-    
-    font48 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 48)
-    font36 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 36)
-    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
-    font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
-    
-    # partial update
-    #296x128
-    logging.info("5.show time")
-       
-    
-    num = 0
-    while (True):
-        time_image = Image.new('1', (epd.height, epd.width), 255)
-        time_draw = ImageDraw.Draw(time_image)
-        time_draw.rectangle((30, 30, 130, 80), fill = 255)
-        time_draw.text((30, 30), time.strftime('%H:%M:%S'), font = font48, fill = 0)
-        epd.display(epd.getbuffer(time_image))
-        time.sleep(10)
-        num = num + 1
-        if(num == 3):
-            break
-            
-    logging.info("Clear...")
-    epd.init(epd.lut_full_update)
-    epd.Clear(0xFF)
-    
+    time.sleep(2)
     logging.info("Goto Sleep...")
     epd.sleep()
     
@@ -59,3 +67,5 @@ except KeyboardInterrupt:
     logging.info("ctrl + c:")
     epd2in9.epdconfig.module_exit()
     exit()
+
+
